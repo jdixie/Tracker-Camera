8a73d1fc5450aabe9c1264e7f58597d316318870
Alton09 <altonqualls@gmail.com>
2016-02-27 21:33:00 -0700

    Added RecordingStoring design doc, and new class

---
+++ app/src/main/java/com/ninjapiratestudios/trackercamera/CameraRecorder.java
@@ -0,0 +1,44 @@
+package com.ninjapiratestudios.trackercamera;
+
+import android.hardware.Camera;
+import android.media.MediaRecorder;
+
+/**
+ * This class will be used to provide an object that will handle all
+ * of the functions associated with the device's video camera.
+ * @author John Qualls
+ * @version 2/27/2016
+ */
+public class CameraRecorder {
+    private Camera camera;
+    private MediaRecorder mediaRecorder;
+
+    /**
+     * Gets the camera resource from the Android system.
+     */
+    public void acquireCameraResource() {
+
+    }
+
+    /**
+     * Initializes the MediaRecorder object in order to prepare Camera for video recording.
+     */
+    public void setupCamera() {
+
+    }
+
+    /**
+     * Starts Camera recording.
+     */
+    public void startRecording() {
+
+    }
+
+    /**
+     * Stops Camera recording.
+     */
+    public void stopRecording() {
+
+    }
+
+}
