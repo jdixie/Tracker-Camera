a46610eb4b8d257992d713cd40fba667b0b10705
bboudra <benboudra@gmail.com>
2016-04-26 13:35:39 -0600

    send intent to external application to take picture, set image to be added to viewgroup

--- app/src/main/java/com/ninjapiratestudios/trackercamera/configurationFragment/ConfigurationFragment.java
+++ app/src/main/java/com/ninjapiratestudios/trackercamera/configurationFragment/ConfigurationFragment.java
@@ -1,12 +1,20 @@
 package com.ninjapiratestudios.trackercamera.configurationFragment;
 
+import android.app.ActionBar;
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.content.pm.PackageManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.ninjapiratestudios.trackercamera.R;
 
@@ -19,14 +27,17 @@ import com.ninjapiratestudios.trackercamera.R;
  * create an instance of this fragment.
  */
 public class ConfigurationFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+//    private static final String ARG_PARAM1 = "param1";
+//    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+//    private String mParam1;
+//    private String mParam2;
+
+    //Request image capture request value
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    ImageView mImageView;
 
     private OnFragmentInteractionListener mListener;
 
@@ -38,33 +49,47 @@ public class ConfigurationFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment ConfigurationFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static ConfigurationFragment newInstance(String param1, String param2) {
+    public static ConfigurationFragment newInstance() {
         ConfigurationFragment fragment = new ConfigurationFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
+    }
+
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getContext().getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+        }
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+//        if (getArguments() != null) {
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+//        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        dispatchTakePictureIntent();
+        mImageView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        container.addView(mImageView);
         return inflater.inflate(R.layout.fragment_configuration, container, false);
     }
 
@@ -97,7 +122,7 @@ public class ConfigurationFragment extends Fragment {
      * fragment to allow an interaction in this fragment to be communicated
      * to the activity and potentially other fragments contained in that
      * activity.
-     * <p/>
+     * <p>
      * See the Android Training lesson <a href=
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
